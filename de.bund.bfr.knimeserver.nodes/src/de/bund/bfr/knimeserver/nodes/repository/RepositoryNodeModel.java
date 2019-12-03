1518674c280b84545e4aab4cfccc0bea3ac70c6c
Miguel <miguelalba89@gmail.com>
2019-12-03 18:15:54 +0100

    Fix loading/saving internal tables in repository backend

--- de.bund.bfr.knimeserver.nodes/src/de/bund/bfr/knimeserver/nodes/repository/RepositoryNodeModel.java
+++ de.bund.bfr.knimeserver.nodes/src/de/bund/bfr/knimeserver/nodes/repository/RepositoryNodeModel.java
@@ -5,6 +5,7 @@ import org.knime.core.node.BufferedDataTableHolder;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeLogger;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.port.PortObject;
@@ -23,6 +24,8 @@ public class RepositoryNodeModel extends AbstractWizardNodeModel<RepositoryViewR
 	private static final PortType[] IN_TYPES = { BufferedDataTable.TYPE, BufferedDataTable.TYPE,
 			FlowVariablePortObject.TYPE };
 	private static final PortType[] OUT_TYPES = { FlowVariablePortObject.TYPE };
+	
+    private static final NodeLogger LOGGER = NodeLogger.getLogger(RepositoryNodeModel.class);
 
 	// internal tables
 	private BufferedDataTable m_linkTable;
@@ -77,11 +80,12 @@ public class RepositoryNodeModel extends AbstractWizardNodeModel<RepositoryViewR
 	protected PortObject[] performExecute(PortObject[] inObjects, ExecutionContext exec) throws Exception {
 
 		final String selectedModel;
-		
+
 		synchronized (getLock()) {
 
 			RepositoryViewRepresentation repr = getViewRepresentation();
 
+			// If the tables in repr are not set, create them from input tables and assign them
 			if (repr.links == null || repr.basicModelInformation == null) {
 				m_linkTable = (BufferedDataTable) inObjects[0];
 				m_modelInformationTable = (BufferedDataTable) inObjects[1];
@@ -89,14 +93,8 @@ public class RepositoryNodeModel extends AbstractWizardNodeModel<RepositoryViewR
 
 				repr.links = m_linkJsonTable;
 				repr.basicModelInformation = m_modelInformationJsonTable;
-				repr.mainColor = peekFlowVariableString("Color-Main");
-				repr.buttonColor = peekFlowVariableString("Color-Button");
-				repr.hoverColor = peekFlowVariableString("Color Hover");
-				repr.title1 = peekFlowVariableString("Titel1");
-				repr.title2 = peekFlowVariableString("Titel2");
-				repr.metadata = peekFlowVariableString("Metadata");
 			}
-			
+
 			selectedModel = getViewValue().selectedModel;
 		}
 
@@ -110,9 +108,42 @@ public class RepositoryNodeModel extends AbstractWizardNodeModel<RepositoryViewR
 		m_linkJsonTable = JSONDataTable.newBuilder().setDataTable(m_linkTable).build(exec);
 		m_modelInformationJsonTable = JSONDataTable.newBuilder().setDataTable(m_modelInformationTable).build(exec);
 	}
+	
+	@Override
+	public RepositoryViewRepresentation getViewRepresentation() {
+		RepositoryViewRepresentation repr = super.getViewRepresentation();
+		
+		synchronized (getLock()) {
+			// set internal tables
+			if ((m_linkTable != null && m_linkJsonTable == null) ||
+					(m_modelInformationTable != null && m_modelInformationJsonTable == null)) {
+				try {
+					createJsonTables(null);
+					repr.links = m_linkJsonTable;
+					repr.basicModelInformation = m_modelInformationJsonTable;
+				} catch (IllegalArgumentException | CanceledExecutionException e) {
+					LOGGER.error("Could not create JSON table: " + e.getMessage(), e);
+				}
+			}
+			
+			repr.mainColor = peekFlowVariableString("Color-Main");
+			repr.buttonColor = peekFlowVariableString("Color-Button");
+			repr.hoverColor = peekFlowVariableString("Color Hover");
+			repr.title1 = peekFlowVariableString("Titel1");
+			repr.title2 = peekFlowVariableString("Titel2");
+			repr.metadata = peekFlowVariableString("Metadata");
+		}
+		
+		return repr;
+	}
 
 	@Override
 	protected void performReset() {
+		// Reset internal tables
+		m_linkTable = null;
+		m_linkJsonTable = null;
+		m_modelInformationTable = null;
+		m_modelInformationJsonTable = null;
 	}
 
 	@Override
@@ -133,13 +164,12 @@ public class RepositoryNodeModel extends AbstractWizardNodeModel<RepositoryViewR
 
 	@Override
 	public BufferedDataTable[] getInternalTables() {
-		// TODO Auto-generated method stub
-		return null;
+		return new BufferedDataTable[] { m_linkTable, m_modelInformationTable };
 	}
 
 	@Override
 	public void setInternalTables(BufferedDataTable[] tables) {
-		// TODO Auto-generated method stub
-
+		m_linkTable = tables[0];
+		m_modelInformationTable = tables[1];
 	}
 }
