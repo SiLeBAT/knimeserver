b2ee965e55440529f01cc78fe8c2b26ff947acbe
Miguel <miguelalba89@gmail.com>
2019-12-04 19:41:23 +0100

    Add ToxicologicalModel

--- de.bund.bfr.knimeserver.nodes/js-src/repository/repository.js
+++ de.bund.bfr.knimeserver.nodes/js-src/repository/repository.js
@@ -370,9 +370,9 @@
 
   class ToxicologicalModel {
 
-    constructor() {
+    constructor(metadata, img) {
       this.menus = this._createMenus();
-      // TODO: this.panels
+      this.panels = this._createPanels(metadata, img);
     }
 
     _createMenus() {
@@ -393,8 +393,36 @@
         { "id": "parameter", "label": "Parameter" },
         { "id": "qualityMeasures", "label": "Quality measures" },
         { "id": "modelEquation", "label": "Model equation" },
-        { "id": "exposure", "label": "Exposure" }]);
-        // TODO: add model plot tab
+        { "id": "exposure", "label": "Exposure" }]) +
+        `<li role="presentation">
+          <a id="plot-tab" href="#plot"
+            aria-controls="plot" role="tab" data-toggle="tab">Model Plot</a>
+        </li>`;
+    }
+
+    _createPanels(metadata, img) {
+      let schema = schemas.toxicologicalModel;
+      return {
+        generalInformation: createSimplePanel("General information", schema.generalInformation, metadata.generalInformation),
+        modelCategory: createSimplePanel("Model category", schema.modelCategory, metadata.modelCategory),
+        author: createComplexPanel("Author", schema.contact, metadata.author),
+        creator: createComplexPanel("Creator", schema.contact, metadata.creator),
+        reference: createComplexPanel("Reference", schema.reference, metadata.reference),
+        scopeGeneral: createSimplePanel("General", schema.scope, metadata.scope),
+        product: createComplexPanel("Product", schema.product, metadata.product),
+        hazard: createComplexPanel("Hazard", schema.hazard, metadata.hazard),
+        population: createComplexPanel("Population", schema.populationGroup, metadata.populationGroup),
+        study: createSimplePanel("Study", schema.study, metadata.study),
+        studySample: createComplexPanel("Study sample", schema.studySample, metadata.studySample),
+        laboratory: createComplexPanel("Laboratory", schema.laboratory, metadata.laboratory),
+        assay: createComplexPanel("Assay", schema.assay, metadata.assay),
+        modelMath: createSimplePanel("Model math", schema.modelMath, metadata.modelMath),
+        parameter: createComplexPanel("Parameter", schema.parameter, metadata.modelMath.parameter),
+        qualityMeasures: createComplexPanel("Quality measures", schema.qualityMeasures, metadata.modelMath.qualityMeasures),
+        modelEquation: createComplexPanel("Model equation", schema.modelEquation, metadata.modelMath.modelEquation),
+        exposure: createComplexPanel("Exposure", schema.exposure, metadata.modelMath.exposure),
+        plot: createPlotPanel(img) 
+      };
     }
   }
 
@@ -1350,7 +1378,7 @@
       } else if (_representation.metadata.modelType === "otherModel") {
         handler = new OtherModel(metadata, image);
       } else if (_representation.metadata.modelType === "toxicologicalModel") {
-        handler = new ToxicologicalModel();
+        handler = new ToxicologicalModel(metadata, image);
       } else if (_representation.metadata.modelType === "doseResponseModel") {
         handler = new DoseResponseModel(metadata, image);
       } else if (_representation.metadata.modelType === "exposureModel") {
